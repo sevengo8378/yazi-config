d9ee9f3cef99f2097700a842083c5d760b4b36b0
byrne.liang <byrne.liang@alo7.com>
2026-03-15 22:29:04 +0800

    增加几个plugins配置

--- init.lua
+++ init.lua
@@ -6,3 +6,13 @@ require("duckdb"):setup({
   minmax_column_width = 21,  -- default: 21
   column_fit_factor = 10.0   -- default: 10.0
 })
+
+th.git = th.git or {}
+th.git.unknown_sign = " "
+th.git.modified_sign = "M"
+th.git.deleted_sign = "D"
+th.git.clean_sign = "✔"
+require("git"):setup {
+  -- Order of status signs showing in the linemode
+  order = 1500,
+}
